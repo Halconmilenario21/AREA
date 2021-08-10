e6dd8ea7debfdf963efaec69c060aae1758b2d93
Halconmilenario21 <88392029+Halconmilenario21@users.noreply.github.com>
2021-08-10 18:27:48 +0200

    Add files via upload
    Version 1.2

--- AREA.cpp
+++ AREA.cpp
@@ -3,6 +3,58 @@
 // #include "getPI.h"
 using namespace std;
 int main() {
-	select();
+	while (true) {
+		char sel;
+		cout << "Calcular el area de una figura geometrica \n";
+		cout << "C = CUADRADO \n";
+		cout << "R = RECTANGULO \n";
+		cout << "T = TRIANGULO \n";
+		cout << "O = ROMBO \n";
+		cout << "D = TRAPECIO \n";
+		cout << "I = CIRCULO \n";
+		cout << "P = POLIGONO REGULAR \n";
+		//		cout << "Perimetros: \n";
+		//		cout << "c = CUADRADO \n";
+		//		cout << "r = RECTANGULO \n";
+		//		cout << "t = TRIANGULO \n";
+		//		cout << "o = ROMBO \n";
+		//		cout << "d = TRAPECIO \n";
+		cout << "OPCIONES ESPECIALES \n";
+		cout << "E = Exit \n";
+		cout << "V = Version \n";
+		cout << "Que figura quieres calcular:  ";
+		cin >> sel;
+		if (sel == 'C') {
+			cuadrado();
+		}
+		else if (sel == 'R') {
+			rectangulo();
+		}
+		else if (sel == 'T') {
+			triangulo();
+		}
+		else if (sel == 'O') {
+			rombo();
+		}
+		else if (sel == 'D') {
+			trapecio();
+		}
+		else if (sel == 'I') {
+			circulo();
+		}
+		else if (sel == 'E') {
+			break;
+		}
+		else if (sel == 'V') {
+			cout << "1.2.20210810 \n";
+		}
+		else if (sel == 'P') {
+			poligonoregular();
+		}
+		else {
+			cout << "Opcion no valida";
+		}
+	}
+
 	return 0;
 }
--- functions.h
+++ functions.h
@@ -4,7 +4,7 @@ using namespace std;
 float pi = 3.1415;
 int cuadrado() {
 	float LadoCuadrado;
-	cout << "Introduce cuanto mide cualquier lado del cuadrado en centimetros: ";
+	cout << "Introduce la medida de cualquier lado del cuadrado: ";
 	cin >> LadoCuadrado;
 	float AreaCuadrado = LadoCuadrado * LadoCuadrado;
 	cout << "El area del cuadrado es " << AreaCuadrado << "\n";
@@ -12,10 +12,10 @@ int cuadrado() {
 }
 int rectangulo() {
 	float baserectangulo;
-	cout << "Intruduce la base del rectangulo en centimetros: ";
+	cout << "Intruduce la medida de la base del rectangulo: ";
 	cin >> baserectangulo;
 	float alturarectangulo;
-	cout << "Introduce la altura del rectangulo en centimetros: ";
+	cout << "Introduce la medida de altura del rectangulo: ";
 	cin >> alturarectangulo;
 	float arearectangulo = baserectangulo * alturarectangulo / 2;
 	cout << "El area del rectangulo es de " << arearectangulo << "\n";
@@ -24,9 +24,9 @@ int rectangulo() {
 int triangulo() {
 	float alturatriangulo;
 	float basetriangulo;
-	cout << "Introduce la base del triangulo en centímetros";
+	cout << "Introduce la medida de la base del triangulo: ";
 	cin >> basetriangulo;
-	cout << "Introduce la altura del triangulo en centimetros";
+	cout << "Introduce la medida de la altura del triangulo:";
 	cin >> alturatriangulo;
 	float areatriangulo = basetriangulo * alturatriangulo / 2;
 	cout << "El area del triangulo es de " << areatriangulo << "\n";
@@ -35,22 +35,22 @@ int triangulo() {
 int rombo() {
 	float diagonalmenor;
 	float diagonalmayor;
-	cout << "Introduce la diagonal menor del rombo en centimetros: ";
+	cout << "Introduce la medida de la diagonal menor del rombo: ";
 	cin >> diagonalmenor;
-	cout << "Introduce la diagonal mayor del rombo en centimetros: ";
+	cout << "Introduce la medida de la diagonal mayor del rombo: ";
 	cin >> diagonalmayor;
 	float arearombo = diagonalmenor * diagonalmayor / 2;
 	cout << "El area del rombo es de " << arearombo << "\n";
 	return 0;
 }
 int trapecio() {
-	cout << "Introduce la base mayor del trapecio en centímetros: ";
+	cout << "Introduce la medida de la base mayor del trapecio: ";
 	float BMayor;
 	cin >> BMayor;
-	cout << "Introduce la base menor del trapecio en centímetros: ";
+	cout << "Introduce la medida de la base menor del trapecio: ";
 	float BMenor;
 	cin >> BMenor;
-	cout << "Introduce la altura del trapecio en centimetros: ";
+	cout << "Introduce la medida de la altura del trapecio en centimetros: ";
 	float alturatrapecio;
 	cin >> alturatrapecio;
 	float AreaTrapecio = (BMayor * BMenor / 2) * alturatrapecio;
@@ -58,10 +58,10 @@ int trapecio() {
 	return 0;
 }
 int poligonoregular() {
-	cout << "Introduce el perimetro del poligono en centímetros: ";
+	cout << "Introduce la medida del perimetro del poligono: ";
 	float Perimetropoligono;
 	cin >> Perimetropoligono;
-	cout << "Introduce el apotema del poligono en centímetros: ";
+	cout << "Introduce la medida del apotema del poligono: ";
 	float Apotemapoligono;
 	cin >> Apotemapoligono;
 	float Areapoligono = (Perimetropoligono * Apotemapoligono / 2);
@@ -69,65 +69,10 @@ int poligonoregular() {
 	return 0;
 }
 int circulo() {
-	cout << "Introduce el radio del circulo en centimetros: ";
-	float radioa;
-	cin >> radioa;
-	float Areacirculo = radioa * radioa * pi;
+	cout << "Introduce la medida del radio del circulo: ";
+	float radioA;
+	cin >> radioA;
+	float Areacirculo = radioA * radioA * pi;
 	cout << "El area del circulo es de " << Areacirculo << "\n";
-	return 0;
-}
-int select() {
-	cout << "ESTE PROGRAMA SIEMPRE DARA LOS RESULTADOS EN CENTIMETROS CUADRADOS \n";
-	while (true) {
-		char sel;
-		cout << "Calcular el area de una figura geometrica \n";
-		cout << "C = CUADRADO \n";
-		cout << "R = RECTANGULO \n";
-		cout << "T = TRIANGULO \n";
-		cout << "O = ROMBO \n";
-		cout << "D = TRAPECIO \n";
-		cout << "I = CIRCULO \n";
-		cout << "P = POLIGONO REGULAR \n";
-//		cout << "Perimetros: \n";
-//		cout << "c = CUADRADO \n";
-//		cout << "r = RECTANGULO \n";
-//		cout << "t = TRIANGULO \n";
-//		cout << "o = ROMBO \n";
-//		cout << "d = TRAPECIO \n";
-		cout << "OPCIONES ESPECIALES \n";
-		cout << "E = Exit \n";
-		cout << "V = Version \n";
-		cout << "Que figura quieres calcular:  ";
-		cin >> sel;
-		if (sel == 'C') {
-			cuadrado();
-		}
-		else if (sel == 'R') {
-			rectangulo();
-		}
-		else if (sel == 'T') {
-			triangulo();
-		}
-		else if (sel == 'O') {
-			rombo();
-		}
-		else if (sel == 'D') {
-			trapecio();
-		}
-		else if (sel == 'I') {
-			circulo();
-		}
-		else if (sel == 'E') {
-			break;
-		}
-		else if (sel == 'V') {
-			cout << "1.0.2021031700000 \n";
-		}else if (sel == 'P') {
-			poligonoregular();
-		}
-		else {
-			cout << "Opcion no valida";
-		}
-	}
 	return 0;
 }
